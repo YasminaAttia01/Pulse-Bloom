0d9ff6a64164e516df1ba105f1ecf90ba12d8ac8
YasminaAttia01 <attiayasmina01@gmail.com>
2024-10-15 01:26:27 +0100

    fixing changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { PatientForm } from "@/components/forms/PatientForm";
-import PasskeyModal from "@/components/PasskeyModal";
+import { PatientForm } from "../components/forms/PatientForm";
+import PasskeyModal from "../components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
-export default function Home({searchParams}:SearchParamProps) {
-const isAdmin = searchParams.admin ==='true';
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
 
   return (
     <div className="flex h-screen max-h-screen">
-      {isAdmin && <PasskeyModal/>}
+      {isAdmin && <PasskeyModal />}
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
